fc0cf5558d53cf382052bac9467935f9104ab1cf
Paul Franco <paulfranco@me.com>
2017-09-09 23:59:32 -0700

    Update call_center.py

--- call_center.py
+++ call_center.py
@@ -1,26 +1,71 @@
-cd class Call(object):
-    def __init__(self,idt,name,phone,time,reason,*args):
-        self.idt = idt
+from datetime import datetime
+
+class Call(object):
+    def __init__(self,name,phone,reason,id):
+        num_of_calls = 0
         self.name = name
         self.phone = phone
-        self.time = time
         self.reason = reason
+        self.id = id
+        self.time = datetime.now().time() 
+        
+
+
     def display(self):
-        print "Unique ID:", self.idt
+        print "Call ID:", self.id
+        print "Time of Call:", self.time
         print "Customer's Name:", self.name
         print "Customer's Phone:", self.phone
-        print "Time of Call:", self.time
         print "Reason for call:", self.reason
-
-call1 = Call(100,"Paul Franco","206-538-8558","10:10","prank call").display()
-
-class CallCenter(Call):
-    def __init__(self,idt,name,phone,time,reason,queue,calls):
-        super(CallCenter,self).__init__(idt,name,phone,time,reason,queue,calls)
-        self.queue = queue
-        self.calls = []
- 
         
-print "///"
-new_call = CallCenter(2345,"Jesus","206-555-555","9:30","Return Item",10,5).display()
+
+class Call_Center(object):
+    def __init__(self):
+        self.calls = [] #Empty List
+        self.next_id = 1
+        
+    def add_call(self,name,phone,reason): #This is what we are passing to Call(object)
+        new_call = Call(name,phone,reason,self.next_id)
+        self.next_id += 1
+        self.calls.append(new_call) #Appends new calls to List
+        #print "working"
+
+    def remove_call(self):
+        del self.calls[1]
+        return self
+        print self.calls
+
+    def queue_size(self):
+        print "There are ", len(self.calls), "calls left"
+        return len(self.calls)
+        
+    def info(self):
+        print "There are ", len(self.calls), "calls left"
+        for caller in self.calls:
+            print caller.name, caller.phone
+
+    def filter(self,phone): #filter by phone number
+        temp = []
+        for caller in self.calls:
+            if caller.phone == phone:
+                temp.append(caller)                
+                print "this is in temp",caller.name
+        return self
+        
+        
+        
+        
+
+
+newuser = Call_Center() #newuser is a database 
+newuser.add_call("Jesus Franco","206-555-5555","test")
+newuser.add_call("Paul Franco","235-333-333","test")
+#newuser.queue_size()
+#newuser.calls[1].display()
+#newuser.remove_call()
+#newuser.queue_size()
+#newuser.queue_size()
+#newuser.filter("206-538-8558")
+newuser.info()
+
 
